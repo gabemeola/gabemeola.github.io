05e2d1c47eead937558856edd55268c54c288d86
gabemeola <gabemeola@gmail.com>
2016-04-15 00:04:40 -0600

    added webpack.config.js file

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+var path = require("path"),
+	webpack = require("webpack"),
+	ExtractTextPlugin = require('extract-text-webpack-plugin'),
+	autoprefixer = require('autoprefixer');
+module.exports = {
+	entry: { //Entry Point for Webpack
+		app: ["./app/app.jsx", "./public/sass/entry.sass"]
+	},
+	output: {
+		path: "public/",
+		filename: "bundle.js" //Bundled Javascript Webpack Spits out.
+	},
+	devServer: { //Allows webpack-dev-server to be live reloaded
+		inline: true,
+		port: 3333,
+		watch: true,
+		hot: true
+	},
+	module: {
+		loaders: [
+			{ //Babel loader for converting ES2015 to ES5
+				test: /\.js?x$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['react', 'es2015']
+				}
+			},
+			{ //Converts SASS to CSS
+				test: /\.sass$/,
+				loader: ExtractTextPlugin.extract('css-loader?sourceMap!postcss-loader!resolve-url!sass-loader?indentedSyntax')
+			},
+			{ //Loads the font files from imports
+				test:  /\.(ttf|eot|woff(2)?)(\?[a-z0-9]+)?$/,
+				loader: 'file-loader?name=./assets/fonts/[name].[ext]&context=./assets'
+			},
+			{ //Optimizes Images
+				test: /\.(jpe?g|png|gif|svg)$/i,
+				loaders: [
+					'file?hash=sha512&digest=hex&name=./assets/min-imgs/[hash].[ext]',
+					'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false&progressive=true'
+				]
+			},
+			{
+				test: /\.html$/,
+				loader: "html"
+			}
+		]
+	},
+	//Config for Post-CSS and AutoPrefixer
+	postcss: [ autoprefixer({ remove: false, browsers: ['last 2 versions'] }) ],
+	plugins: [
+		new ExtractTextPlugin("main.css")
+	]
+};
