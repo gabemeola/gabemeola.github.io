a79b63db7a6131f813db7e6ef5b27fe775281764
gabemeola <gabemeola@gmail.com>
2016-06-01 14:22:32 -0600

    updated to proper React structuring

--- app/config/routes.jsx
+++ app/config/routes.jsx
@@ -1,3 +1,10 @@
 import React from "react";
 import { Route, IndexRoute } from "react-router";
 
+const routes = (
+	<Route path="/" component={App}>
+		<IndexRoute component={Navbar}/>
+	</Route>
+);
+
+export default routes;
