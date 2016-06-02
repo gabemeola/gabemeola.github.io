8149cf500b5ec1c3af138518f5da502cfecdeec2
gabemeola <gabemeola@gmail.com>
2016-06-02 17:14:48 -0600

    MenuNav is working

--- app/components/MenuNav.jsx
+++ app/components/MenuNav.jsx
@@ -8,16 +8,18 @@ function Menu(props) {
 				<Link to="/">
 					<h2>About me</h2>
 				</Link>
-				<h2>Work</h2>
-				<Link to="work/cosmeticlaserexchange">
-					<h3>Cosmetic Laser Exchange</h3>
-				</Link>
-				<Link to="work/5starlegal">
-					<h3>5 Star Legal</h3>
-				</Link>
-				<Link to="work/cam-taylor">
-					<h3>Cameron & Taylor Wedding</h3>
-				</Link>
+				<div className="menu-nav-works-wrapper">
+					<h2>Work</h2>
+					<Link to="work/cosmeticlaserexchange">
+						<h3 className="menu-nav-works">Cosmetic Laser Exchange</h3>
+					</Link>
+					<Link to="work/5starlegal">
+						<h3 className="menu-nav-works">5 Star Legal</h3>
+					</Link>
+					<Link to="work/cam-taylor">
+						<h3 className="menu-nav-works">Cameron & Taylor Wedding</h3>
+					</Link>
+				</div>
 			</div>
 		</div>
 	)
