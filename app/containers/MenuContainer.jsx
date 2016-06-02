c524759597857076922b6c0663ac5de96eb198ef
gabemeola <gabemeola@gmail.com>
2016-06-02 16:11:57 -0600

    fixed side spacing

--- app/containers/MenuContainer.jsx
+++ app/containers/MenuContainer.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Menu from "../components/Menu";
 
 class MenuContainer extends React.Component {
 	render() {
 		return (
 			<div className="menu-container">
+				<Menu/>
 			</div>
 		)
 	}
