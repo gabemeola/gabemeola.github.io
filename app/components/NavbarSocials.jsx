4794049d7d873d960fa38f216f43cc6a1204a242
gabemeola <gabemeola@gmail.com>
2016-06-02 10:19:56 -0600

    imported in assets and applied flexbox

--- app/components/NavbarSocials.jsx
+++ app/components/NavbarSocials.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import FacebookIcon from "../../assets/Facebook.svg";
+import TwitterIcon from "../../assets/Twitter.svg";
+import GithubIcon from "../../assets/Github.svg";
 
 function NavbarSocials(props) {
 	return(
 		<div className="navbar-socials">
-			<h4 className="navbar-socials-facebook">Facebook</h4>
-			<h4 className="navbar-socials-twitter">Twitter</h4>
-			<h4 className="navbar-socials-github">Github</h4>
+			<img className="navbar-socials-facebook" src={FacebookIcon} alt="Facebook Link"/>
+			<img className="navbar-socials-twitter" src={TwitterIcon} alt="Twitter Link"/>
+			<img className="navbar-socials-github" src={GithubIcon} alt="Github Link"/>
 		</div>
 	)
 }
