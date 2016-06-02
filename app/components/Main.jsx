51eac3370ea9f377fe0fc6465ecf63da33856e98
gabemeola <gabemeola@gmail.com>
2016-06-02 15:05:48 -0600

    added MenuContainer and change container to containers

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import NavbarContainer from "../container/NavbarContainer";
+import NavbarContainer from "../containers/NavbarContainer";
 
 class Main extends React.Component {
 	render() {
--- app/config/routes.jsx
+++ app/config/routes.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Router, hashHistory, Route, IndexRoute } from "react-router";
 import Main from "../components/Main";
 import Home from "../components/Home";
+import MenuContainer from "../containers/MenuContainer";
 
 const routes = (
 	<Router history={hashHistory}>
 		<Route path="/" component={Main}>
 			<IndexRoute component={Home}/>
+			<Route path="menu"/>
 		</Route>
 	</Router>
 );
