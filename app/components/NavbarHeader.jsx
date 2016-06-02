16c2709755594cd33d32b636151584c79de7b13f
gabemeola <gabemeola@gmail.com>
2016-06-02 01:42:16 -0600

    added in styling for navbarheader

--- app/components/NavbarHeader.jsx
+++ app/components/NavbarHeader.jsx
@@ -3,6 +3,7 @@ import React from "react";
 function NavbarHeader (props) {
 	return(
 		<div className="navbar-header-wrapper">
+			<div className="navbar-header-block"></div>
 			<h1 className="navbar-header-text">gabemeola.com</h1>
 		</div>
 	)
