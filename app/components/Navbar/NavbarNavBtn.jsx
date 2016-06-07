0c59169820d9e088229f8f532de7fba3ebfbac8a
gabemeola <gabemeola@gmail.com>
2016-06-07 10:17:17 -0600

    added navbar button switcher

--- app/components/Navbar/NavbarNavBtn.jsx
+++ app/components/Navbar/NavbarNavBtn.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { PropTypes } from "react";
 import navbarIcon from "../../../assets/navbar.svg";
 import { Link } from "react-router";
 
 function NavbarNavBtn(props) {
 	return(
 		<div className="navbar-navbtn">
-			<Link to="menu">
+			<a onClick={props.onNavbarOpen}>
 				<img src={navbarIcon} alt="Navigation Icon"/>
-			</Link>
+			</a>
 		</div>
 	)
 }
+
+NavbarNavBtn.propTypes = {
+	onNavbarOpen: PropTypes.func.isRequired,
+	isNavbarOpen: PropTypes.bool.isRequired
+};
 
 export default NavbarNavBtn;
--- app/containers/Navbar/NavbarContainer.jsx
+++ app/containers/Navbar/NavbarContainer.jsx
@@ -2,15 +2,34 @@ import React from "react";
 import NavbarHeader from "../../components/Navbar/NavbarHeader";
 import NavbarNavBtn from "../../components/Navbar/NavbarNavBtn";
 
-class Navbar extends React.Component {
+class NavbarContainer extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			navbarOpen: false
+		}
+	}
+	handleNavbarOpen() {
+		this.state.navbarOpen ?
+		this.setState({
+			navbarOpen: false
+		}) :
+			this.setState({
+				navbarOpen: true
+			});
+		console.log(this.state.navbarOpen);
+	}
 	render() {
 		return(
 			<div className="navbar">
 				<NavbarHeader/>
-				<NavbarNavBtn/>
+				<NavbarNavBtn
+					onNavbarOpen={() => this.handleNavbarOpen()}
+				  isNavbarOpen={this.state.navbarOpen}
+				/>
 			</div>
 		)
 	}
 }
 
-export default Navbar;
+export default NavbarContainer;
