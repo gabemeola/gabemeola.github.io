b0a739429a15ffbc0af0cc8dff7d4c653b99099c
gabemeola <gabemeola@gmail.com>
2016-06-10 11:38:12 -0600

    moved LandingIcon styles to new sas

--- app/components/Landing/Landing.jsx
+++ app/components/Landing/Landing.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import LandingIcon from "./LandingIcon";
 
 function Landing(props) {
 	return(
 		<div className="landing-wrapper">
 			<h1><div className="underline">Hello!<div className="underline-mark"></div></div> - Gabe</h1>
-			<LandingIcon/>
 		</div>
 	)
 }
--- app/containers/Landing/LandingContainer.jsx
+++ app/containers/Landing/LandingContainer.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Landing from "../../components/Landing/Landing";
+import LandingIcon from "../../components/Landing/LandingIcon";
 
 class LandingContainer extends React.Component {
 	render() {
 		return(
 			<div className="landing">
 				<Landing/>
+				<LandingIcon/>
 			</div>
 		)
 	}
