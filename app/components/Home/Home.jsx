cb11f23e58d2e29fbdb4871477a5f21c2b3e88e3
gabemeola <gabemeola@gmail.com>
2016-06-12 19:05:26 -0600

    added home component fade menu

--- app/components/Home/Home.jsx
+++ app/components/Home/Home.jsx
@@ -4,6 +4,12 @@ function Home(props) {
 	return(
 		<div className="home-wrapper">
 			<div className="home-jumbotron"></div>
+			<div className="home-content-wrapper">
+				<div className="home-content-header"><h3>YO YO YO</h3></div>
+				<div className="home-content">
+					<h3>Bang Bang Bang</h3>
+				</div>
+			</div>
 		</div>
 	)
 }
