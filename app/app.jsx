f092818be76a491cca9ac10d185f7a7a4f95b80c
gabemeola <gabemeola@gmail.com>
2016-06-14 11:37:44 -0600

    added preloader fade-animation

--- app/app.jsx
+++ app/app.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import routes from "./config/routes";
 
-window.onload = () => { // Wait for CSS to load and externals
-	ReactDOM.render(
-		routes,
-		document.getElementById("app")
-	);
-};
+// window.onload = () => { // Wait for CSS to load and externals
+// 	ReactDOM.render(
+// 		routes,
+// 		document.getElementById("app")
+// 	);
+// };
