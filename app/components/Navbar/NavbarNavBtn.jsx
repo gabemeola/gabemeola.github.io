4787c20047a61142392631c90ff32de824d18f12
gabemeola <gabemeola@gmail.com>
2016-06-29 20:35:10 -0600

    added extra padding around navbar-btn

--- app/components/Navbar/NavbarNavBtn.jsx
+++ app/components/Navbar/NavbarNavBtn.jsx
@@ -2,9 +2,11 @@ import React, { PropTypes } from "react";
 
 function NavbarNavBtn(props) {
 	return(
-		<div onClick={props.onNavbarSwitch} className={"navbar-navbtn "  + (props.isNavbarOpen ? "navbar-navbtn--open" : "")}>
-			<div className={"navbar-navbtn-img1 " + (props.isNavbarOpen ? "navbar-navbtn-img1--open" : "")}></div>
-			<div className={"navbar-navbtn-img2 " + (props.isNavbarOpen ? "navbar-navbtn-img2--open" : "")}></div>
+		<div className="navbar-navbtn-wrapper" onClick={props.onNavbarSwitch}>
+			<div className={"navbar-navbtn "  + (props.isNavbarOpen ? "navbar-navbtn--open" : "")}>
+				<div className={"navbar-navbtn-img1 " + (props.isNavbarOpen ? "navbar-navbtn-img1--open" : "")}></div>
+				<div className={"navbar-navbtn-img2 " + (props.isNavbarOpen ? "navbar-navbtn-img2--open" : "")}></div>
+			</div>
 		</div>
 	)
 }
