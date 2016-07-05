3feb0fba89c67fbad8b883e4e5bccd0bb815423d
gabemeola <gabemeola@gmail.com>
2016-07-05 16:03:45 -0600

    basic styling changes

--- app/components/Landing/Landing.jsx
+++ app/components/Landing/Landing.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Landing(props) {
 	return(
 		<div className="landing-wrapper">
-			<h1><div className="underline">Hello!<div className="underline-mark"></div></div> - Gabe</h1>
+			<h1><div className="underline">Hello!<div className="underline-mark"></div></div>&nbsp;-&nbsp;Gabe</h1>
 		</div>
 	)
 }
