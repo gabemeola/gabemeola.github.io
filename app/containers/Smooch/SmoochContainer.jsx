3537cf5848342fba9dc62ff518422bfe551c2e56
gabemeola <gabemeola@gmail.com>
2016-07-15 01:12:16 -0600

    currently working on smooch conversations

--- app/containers/Smooch/SmoochContainer.jsx
+++ app/containers/Smooch/SmoochContainer.jsx
@@ -1,12 +1,64 @@
 import React from "react";
-import * as smoochUtils from "../../utils/smoochUtils";
+import { initSmooch, postSmooch, getSmooch } from "../../utils/smoochUtils";
 
-smoochUtils.init("bob@example.com");
+function Conversation({data}) {
+	return (
+		<div>
+			{/*{data[0].text}*/}
+		</div>
+	)
+}
 
 class SmoochContainer extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			inputText: "",
+			smoochConversation: []
+		}
+	}
+	componentWillMount() {
+		initSmooch("bob@example.com")
+			.then((res) => {
+				getSmooch().then((res) => {
+					this.setState({
+						smoochConversation: res.conversation.messages
+					})
+				})
+			});
+	}
+	handleTextSubmit(e) {
+		e.preventDefault();
+		postSmooch(this.state.inputText)
+			.then(() => {
+				getSmooch().then((res) => {
+					this.setState({
+						smoochConversation: res.conversation.messages
+					})
+				});
+			});
+		this.setState({
+			inputText: ""
+		})
+	}
 	render() {
 		return (
-			<div>Smooch Container</div>
+			<div>
+				<h2>Smooch Container</h2>
+				<br/>
+				<Conversation
+					data={this.state.smoochConversation}
+				/>
+				{this.state.smoochConversation.forEach((item) => console.warn(item))}
+				<br/>
+				<form onSubmit={(event) => this.handleTextSubmit(event)}>
+					<input
+						value={this.state.inputText}
+						onChange={(event) => this.setState({inputText: event.target.value})}
+					  type="text"
+					/>
+				</form>
+			</div>
 		)
 	}
 }
--- app/utils/smoochUtils.js
+++ app/utils/smoochUtils.js
@@ -1,19 +1,50 @@
 import SmoochCore from "smooch-core"
 import createHash from "./createHash";
+import q from "q";
 
+let smoochUserEmail;
 let smooch = new SmoochCore({
 	appToken: "3nf36hcp4oj7ab8f52uvmkleb"
 });
 
-export function init(email) {
-	let smoochId = createHash(email + window.navigator.platform, true);
-	console.log(smoochId);
+
+export function initSmooch(email) {
+	let defer = q.defer();
+	let deviceId = createHash(email + window.navigator.platform, true);
+	console.log(deviceId);
 	smooch.appUsers.init({
 		device: {
-			id: smoochId,
+			id: deviceId,
 			platform: 'web',
 			appVersion: '1.0'
 		},
 		userId: email
-	}).then((res) => console.log("Smooch returns: ", res));
+	}).then((res) => {
+		console.log("Smooch returns: ", res);
+		smoochUserEmail = res.appUser._id;
+		defer.resolve(res);
+	});
+	return defer.promise;
+}
+
+export function postSmooch(text) {
+	let defer = q.defer();
+	smooch.conversations.sendMessage(smoochUserEmail, {
+		text: text,
+		role: "appUser"
+	}).then((res) => {
+		console.log(res);
+		defer.resolve(res);
+	});
+	return defer.promise;
+}
+
+export function getSmooch() {
+	let defer = q.defer();
+	smooch.conversations.get(smoochUserEmail)
+		.then((res) => {
+			console.log("Get Smooch: ", res);
+			defer.resolve(res);
+		});
+	return defer.promise;
 }
