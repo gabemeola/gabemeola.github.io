bb0450e196649efdf427fd67bae57cbbc1cd80d3
gabemeola <gabemeola@gmail.com>
2016-07-19 23:36:51 -0600

    returned SmoochChats Component back to a stateless component

--- app/components/Smooch/SmoochChats.jsx
+++ app/components/Smooch/SmoochChats.jsx
@@ -1,30 +1,22 @@
 import React, { PropTypes } from 'react';
 
-class SmoochChats extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-	componentDidMount() {
-		window.scrollTo(0,document.body.scrollHeight);
-	}
-	render() {
-		const convo = this.props.conversation.map((info, index) => {
-			return (
-				<p
-					key={index}
-					className={"smooch-chat-thread " + (info.role == "appUser" ? "smooch-chat-thread--appUser" : "smooch-chat-thread--appMaker")}
-				>
-					{info.text}<span className="smooch-chat-name">{info.name}</span>
-				</p>
-			);
-		});
-
+function SmoochChats({conversation}) {
+	const convo = conversation.map((info, index) => {
 		return (
-			<div className="smooch-chat">
-				{convo}
-			</div>
-		)
-	}
+			<p
+				key={index}
+				className={"smooch-chat-thread " + (info.role == "appUser" ? "smooch-chat-thread--appUser" : "smooch-chat-thread--appMaker")}
+			>
+				{info.text}<span className="smooch-chat-name">{info.name}</span>
+			</p>
+		);
+	});
+
+	return (
+		<div className="smooch-chat">
+			{convo}
+		</div>
+	)
 }
 
 SmoochChats.propTypes = {
