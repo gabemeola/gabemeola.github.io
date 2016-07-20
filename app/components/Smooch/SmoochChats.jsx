f202a47376660a89bbab4e8961b62f0ec95d9048
gabemeola <gabemeola@gmail.com>
2016-07-19 23:30:57 -0600

    updated smooch chat styling

--- app/components/Smooch/SmoochChats.jsx
+++ app/components/Smooch/SmoochChats.jsx
@@ -1,24 +1,30 @@
 import React, { PropTypes } from 'react';
 
-function SmoochChats({conversation}) {
+class SmoochChats extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	componentDidMount() {
+		window.scrollTo(0,document.body.scrollHeight);
+	}
+	render() {
+		const convo = this.props.conversation.map((info, index) => {
+			return (
+				<p
+					key={index}
+					className={"smooch-chat-thread " + (info.role == "appUser" ? "smooch-chat-thread--appUser" : "smooch-chat-thread--appMaker")}
+				>
+					{info.text}<span className="smooch-chat-name">{info.name}</span>
+				</p>
+			);
+		});
 
-	const convo = conversation.map((info, index) => {
 		return (
-			<p
-				key={index}
-				className={"smooch-chat-thread " + (info.role == "appUser" ? "smooch-chat-thread--appUser" : "smooch-chat-thread--appMaker")}
-			>
-				{info.text} - <span className="smooch-chat-name">{info.name}</span>
-			</p>
-		);
-	});
-
-	return (
-		<div className="smooch-chat">
-			<h3>Smooch Chats</h3>
-			{convo}
-		</div>
-	)
+			<div className="smooch-chat">
+				{convo}
+			</div>
+		)
+	}
 }
 
 SmoochChats.propTypes = {
