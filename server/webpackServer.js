ce1b1e4c73008ec619fce5a73d9ffe8ab0d2e74c
gabemeola <gabemeola@gmail.com>
2016-09-04 08:28:06 -0600

    updated proxy config to new api

--- server/webpackServer.js
+++ server/webpackServer.js
@@ -5,20 +5,21 @@ var webpack = require("webpack"),
 module.exports = (PORT, ADDRESS) => {
 	const server = new WebpackDevServer(webpack(config), {
 		proxy: {
-			"/" : {
+			"**" : {
 				target: `http://${ADDRESS}:${PORT - 10}`,
 				secure: false
 			}
 		},
 		progress: true,
 		hot: true,
+		historyApiFallback: false,
 		stats: {
 			progress: true,
 			colors: true
 		}
 	});
 	server.listen(PORT, '0.0.0.0', function(err, result) {
-		if(err) return console.warn(err);
+		if(err) throw console.warn(err);
 	});
 	console.log(`Webpack Server starting at: http://${ADDRESS}:${PORT}`)
 };
