1dd3ed02cf7ba0a40a73a4425a475f243ab193d5
gabemeola <gabemeola@gmail.com>
2016-09-22 20:39:27 -0600

    added index.js exports for container and components

---
+++ app/containers/index.js
@@ -0,0 +1,7 @@
+export { default as HomeContainer } from './Home/HomeContainer';
+export { default as LandingContainer } from './Landing/LandingContainer';
+export { default as MenuContainer } from './Menu/MenuContainer';
+export { default as NavbarContainer } from './Navbar/NavbarContainer';
+export { default as NotificationContainer } from './Notification/NotificationContainer';
+export { default as SmoochContainer } from './Smooch/SmoochContainer';
+export { default as WorksContainer } from './Works/WorksContainer';
