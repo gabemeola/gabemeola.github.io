6aa9adebf13fad10af4255190ec0fddf0ba391eb
gabemeola <gabemeola@gmail.com>
2016-10-09 15:45:19 -0600

    added onTouchStart to reduce delay on mobile

--- app/components/Navbar/NavbarNavBtn.jsx
+++ app/components/Navbar/NavbarNavBtn.jsx
@@ -2,7 +2,7 @@ import React, { PropTypes } from "react";
 
 function NavbarNavBtn({navSwitch, isNavOpen}) {
 	return(
-		<div className="navbar-navbtn-wrapper" onClick={navSwitch}>
+		<div className="navbar-navbtn-wrapper" onClick={navSwitch} onTouchStart={() => {}}>
 			<div className={"navbar-navbtn "  + (isNavOpen ? "navbar-navbtn--open" : "")}>
 				<div className={"navbar-navbtn-img1 " + (isNavOpen ? "navbar-navbtn-img1--open" : "")}></div>
 				<div className={"navbar-navbtn-img2 " + (isNavOpen ? "navbar-navbtn-img2--open" : "")}></div>
